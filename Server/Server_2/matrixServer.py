9d41b6f3bd0148c391aafe86e0717321772db701
dlewin <dlewin@free.fr>
2014-09-10 17:09:18 +0200

    Update matrixServer.py
    some clean up

--- Server/Server_2/matrixServer.py
+++ Server/Server_2/matrixServer.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python -O
 
 import sys
-sys.path.append('..')                       # Adafruit libs are in previous dir
+sys.path.append('..')                          # Adafruit libs are in previous dir
 
-from Adafruit_8x8 import ColorEightByEight
-from twisted.internet import reactor, protocol
+from Adafruit_8x8 import ColorEightByEight     # import the color version of the matrix lib
+from twisted.internet import reactor, protocol # we use the twisted matrix lib
 import time
 
-grid = ColorEightByEight(address=0x70)
+grid = ColorEightByEight(address=0x70)         # create a grid object to represent the matrix
 
 class Echo(protocol.Protocol ):
     def dataReceived(self, data):
         if data.find("cls") <> -1 :
             print "clear matrix"
             clear_matrix()
-        else:           
-             x= iter(data)
-             for x, y, color in zip(*([x] * 3)):
-                 y7= 7 - ( int(y) )
-                 x7= 7 - (int(x) )
-                 grid.setPixel( int(y),int(x7), int(color) )
-                 print y,x7
+        else:                                  # a data has been received, let's use it
+             x= iter(data)                     # iterate over the received data
+             for x, y, color in zip(*([x] * 3)): # the data itself relies on x,y and a color attribute
+                 y7= 7 - ( int(y) )            # an adapation to represent the same coordinates 
+                 x7= 7 - (int(x) )             # as on the client screen (letft/top)
+                 grid.setPixel( int(y),int(x7), int(color) )  # light the led
 
 def Clear_col(col):
 	for i in range(0,8):
 	   grid.setPixel(i,col,0)
 
 def Bargraph(col,row):
-    col_pos = 7 - col  # no need to compute it on each loop
+    col_pos = 7 - col  				# no need to compute it on each loop
     Clear_col(col_pos)
-    print("----------------------")
+    
     if ( row >0 and row <8 ):
         index=0
     	while ( index <row ):
             grid.setPixel( ( 7 - index ) , col_pos, 1 )
-            print("index : ",index," row ",row," 7-index: ",(7-index))
             index = index+1
     else :
             print ("Out of matrix range")
